886282357d3b32faf6bf59b6d5c1461d974e4cbd
jiholee <stellar7484@naver.com>
2021-09-10 13:03:08 +0900

    add game, bank homePage jobPage

--- myfirstmap/public/data/data.js
+++ myfirstmap/public/data/data.js
@@ -1,16 +1,177 @@
-var data = [
+var game = [
 	{
-	  title : "서울", 
-	  content: "서울 남산", 
-	  date : "2020-07-10", 
-	  lat : 37.55504960115933,
-	  lng: 126.98114112443173
-	},  
+	  company : "크래프톤", 
+	  address: "경기도 성남시 분당내곡로 117 KR 크래프톤 타워", 
+	  date : "2020-07-10",
+	  homePage: "http://www.krafton.com/",
+	  jobPage: "https://www.krafton.com/careers/jobs/",
+	  lat : 37.394391818921406,
+	  lng: 127.11206414870664
+	},
 	{
-	  title : "서울", 
-	  content: "서울 용산", 
+	  company : "네오위즈", 
+	  address: "경기도 성남시 분당구 삼평동 대왕판교로645번길", 
 	  date : "2020-07-11", 
-	  lat : 37.5254148, 
-	  lng: 126.9798002
+	  homePage: "http://www.neowiz.co.kr/",
+	  jobPage: "https://neowiz.recruiter.co.kr/appsite/company/index",
+	  lat : 37.40038888075271,
+	  lng: 127.10419619789263
+	},
+	{
+		company : "컴투스", 
+		address: "서울특별시 금천구 가산디지털1로", 
+		date : "2020-07-11", 
+		homePage: "https://www.withhive.com/",
+		jobPage: "https://com2us.recruiter.co.kr/app/jobnotice/list",
+		lat : 37.478143627636996,
+		lng: 126.88277233723957
+	},
+	{
+		company : "넥슨 코리아", 
+		address: "경기도 성남시 분당구 삼평동 판교로 256번길 7", 
+		date : "2020-07-11",
+		homePage: "http://www.nexon.com/",
+		jobPage: "https://career.nexon.com/common/main",
+		lat : 37.402306468958585, 
+		lng: 127.1034927418025
+	},
+	{
+		company : "네오플", 
+		address: "제주특별자치도 제주시 노형동 1100로 3198-13", 
+		date : "2020-07-11", 
+		homePage: "http://www.neople.co.kr/",
+		lat : 33.47288708023531,
+		lng: 126.48513500379474
+	},
+	{
+		company : "엔씨소프트 판교R&D센터", 
+		address: "경기도 성남시 분당구 삼평동 대왕판교로644번길 12", 
+		date : "2020-07-11", 
+		homePage: "http://kr.ncsoft.com/korean/",
+		jobPage: "https://careers.ncsoft.com/apply/list",
+		lat : 37.39976208260625, 
+		lng: 127.10877548598339
+	},
+	{
+		company : "넷마블", 
+		address: "서울특별시 구로구 구로동 디지털로26길 38 G-Tower", 
+		date : "2020-07-11", 
+		homePage: "http://www.netmarble.net/",
+		jobPage: "https://company.netmarble.com/rem/www/noticelist.jsp",
+		lat : 37.480287318028495, 
+		lng: 126.89420685344302
+	},
+	{
+		company : "펍지", 
+		address: "서울 서초구 서초대로38길12 마제스타시티 7층", 
+		date : "2020-07-11", 
+		homePage: "https://www.pubg.com/ko/",
+		jobPage: "https://careers.pubg.com/?_ga=2.124807491.878416622.1631246038-2006097136.1631246038#/ko/seoul",
+		lat : 37.49062397193703, 
+		lng: 127.00537514840038
+	},
+	{
+		company : "스마일게이트엔터테인먼트", 
+		address: "경기도 성남시 분당구 판교역로 220(삼평동) 쏠리드 스페이스 빌딩 5층", 
+		date : "2020-07-11", 
+		homePage: "https://www.smilegate.com/ko/main/main.asp",
+		jobPage: "https://careers.smilegate.com/ko/recruit/recruit_list.asp",
+		lat : 37.40062941075254, 
+		lng: 127.11068473994904
+	},
+	{
+		company : "펄어비스", 
+		address: "경기도 안양시 동안구 관양동 시민대로327번길 24", 
+		date : "2020-07-11", 
+		homePage: "http://www.pearlabyss.com/",
+		jobPage: "https://pearlabyss-recruit.saramin.co.kr/apply_site/recruit/list",
+		lat : 37.40417889902762, 
+		lng: 126.96607587819015
+	},
+	{
+		company : "NHN", 
+		address: "경기도 성남시 삼평동 대왕판교로645번길 16 NHN 플레이뮤지엄", 
+		date : "2020-07-11", 
+		homePage : "https://www.nhn.com/",
+		jobPage : "https://recruit.nhn.com/ent/index", 
+		lat : 37.40139907438103, 
+		lng: 127.1041703213399
 	}
-]
+];
+
+var banking = [
+	{
+		company : "카카오뱅크", 
+		address: "경기도 성남시 분당구 판교역로 231, S동 5층 (삼평동, H스퀘어)", 
+		date : "2020-07-10", 
+		homePage : "https://www.kakaobank.com/",
+		jobPage : "https://kakaobank.recruiter.co.kr/appsite/company/index",
+		lat : 37.401359624784796, 
+		lng: 127.10869564145064
+	},
+	{
+		company : "비바리 퍼블리카", 
+		address: "서울특별시 강남구 역삼동 테헤란로 142", 
+		date : "2020-07-10",
+		homePage : "https://toss.im/",
+		jobPage : "https://toss.im/career",
+		lat : 37.499781344868396,
+		lng: 127.03494982606796
+	},
+	{
+		company : "뱅크 샐러드", 
+		address: "서울특별시 영등포구 여의동 국제금융로8길 16 9층", 
+		date : "2020-07-10",
+		homePage : "https://www.banksalad.com/",
+		jobPage: "https://career.banksalad.com/jobs/",
+		lat : 37.52147832216816,
+		lng : 126.92891421257622
+	},
+	{
+		company : "파운트", 
+		address: "서울 서대문구 충정로 50", 
+		date : "2020-07-10",
+		homePage: "https://fount.co/",
+		jobPage: "https://fount.co/career",
+		lat : 37.563218380053826,
+		lng : 126.96490139723416
+	},
+	// {
+	// 	company : "페이북", 
+	// 	address: "서울특별시 중구 회현동 을지로 170", 
+	// 	date : "2020-07-10",
+	// 	homePage: "https://www.paybooc.co.kr/app/paybooc/Main.do",
+	// 	jobPage : "https://www.bccard.com/card/html/company/kr/recruit/recruit/announce/index.jsp",
+	// 	lat : 37.56324389368788, 
+	// 	lng : 126.96484775305507
+	// },
+	{
+		company : "우리은행", 
+		address: "서울특별시 중구 회현동 소공로 51", 
+		date : "2020-07-10",
+		homePage : "https://www.wooribank.com/",
+		jobPage : "https://spot.wooribank.com/pot/Dream?withyou=BPHRR0001",
+		lat : 37.55954526435726, 
+		lng : 126.98173249723409
+	},
+	{
+		company : "국민은행", 
+		address: "서울특별시 영등포구 여의동 45", 
+		date : "2020-07-10",
+		homePage : "https://www.kbstar.com/",
+		jobPage : "https://kbstar.incruit.com/index_kbstar.asp",
+		lat : 37.5195411801499,
+		lng : 126.92748197024784
+	},
+	{
+		company : "신한은행", 
+		address: "서울특별시 중구 태평로2가 세종대로9길 20", 
+		date : "2020-07-10",
+		homePage : "https://www.shinhan.com/index.jsp",
+		jobPage : "https://shinhan.recruiter.co.kr/appsite/company/index",
+		lat : 37.56112279964291, 
+		lng : 126.9745585234565
+	}
+];
+
+var data = [...game, ...banking];
